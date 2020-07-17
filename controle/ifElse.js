f45266a02e9f605ea3a1fb650382e4400d64062e
Guilherme Augusto <augusto.gui.2208@gmail.com>
2020-07-17 18:55:13 -0300

    Tudo que aprendi hoje

--- controle/ifElse.js
+++ controle/ifElse.js
@@ -1,6 +1,7 @@
 const imprimirResultado = function (nota) {
   if(nota >= 7) {
     console.log('Aprovado!')
+    //console.log(`Aprovado com  nota ${nota}`)
   } else {
     console.log('Reprovado!')
   }
