661ed07009ef942d0aa9f9b85823189153368bd4
Guilherme Augusto <augusto.gui.2208@gmail.com>
2020-09-01 17:24:15 -0300

    Aprendi bastante coisa hoje

---
+++ JavaScript Fundamentos/objeto/oo.js
@@ -0,0 +1,22 @@
+// NON-EXECUTABLE CODE!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+//  OO     in OO I have data, and within that data I have functions  na oo eu tenho dados, e dentro desse dados eu tenho funções
+
+obejto = {
+  valor1,
+  valor2,
+  valor3,
+  processamento() {
+    // ...
+  }
+}
+
+objeto.processamento //  The Focus became the object    O Foco passou a ser o objeto
+
+// important principle.           
+// 1. abstraction  //  abstraction is when you try to take an object in real life, and put it in your system 
+// 2. enclosure and have the details of hidden implementations and you show a simple interface to be able to interact with certain object     encupsulamento e ter os detalhes de implementações escondidos e você mostrar uma interface simples para poder interagir com determinados objeto  
+// 3. heritage (prototype) and a principle based on a relation is 1      herança (prototype) e um principio baseado em uma relação é 1
