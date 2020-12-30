a83ecd694410137d1958aae718bdf06b211748fc
Guilherme Augusto <68301964+Guilherme-Santos08@users.noreply.github.com>
2020-12-29 21:04:48 -0300

    Let

---
+++ JavaScript Fundamentos/1-Fundamentos/17-usandoVarEmLoop.js
@@ -0,0 +1,5 @@
+for (var i = 0; i < 10; i++) { // i++ Cada repetição o i sera incrementado, até chegar no 10
+  console.log(i)
+} 
+
+console.log('i =', i) // O valor de i será o valor que fez ele sair do laço, então será 10
---
+++ JavaScript Fundamentos/1-Fundamentos/18- usandoLetEmLoop1.js
@@ -0,0 +1,4 @@
+for (let i = 0; i <10; i++) { // i++ Cada repetição o i sera incrementado, até chegar no 10
+  console.log(i)
+}
+console.log('i =', i) // Ao contrario do var, o i so estara visiveil dentro do bloco acima
