ef5a7e42f3a67d0511d84e10cd21eae100dbce7a
Guilherme Augusto <68301964+Guilherme-Santos08@users.noreply.github.com>
2020-12-30 18:51:01 -0300

    aritimeticos

---
+++ JavaScript Fundamentos/1-Fundamentos/28- aritimeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [3, 5, 1, 15] //Aqui vamos usar os operadores destructuring
+
+const soma = a + b + c + d // 24
+const subtracao = d - b // 10
+const multiplicacao = a * b  //15
+const divisao = d / a // -5
+const modulo = a % 2 //1
+
+console.log(soma, subtracao, multiplicacao, -divisao, modulo)
