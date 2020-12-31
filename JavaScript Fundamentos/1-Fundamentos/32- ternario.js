0486bd145897e134fb563fd6f46cad7c9c945b78
Guilherme Augusto <68301964+Guilherme-Santos08@users.noreply.github.com>
2020-12-31 10:16:49 -0300

    ternario

--- JavaScript Fundamentos/1-Fundamentos/32- ternario.js
+++ JavaScript Fundamentos/1-Fundamentos/32- ternario.js
@@ -1,6 +1,7 @@
+//    1 parte                             2 parte
 const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+// 3 parte
+console.log(resultado(7.1))
+console.log(resultado(6.1))
 
-console.log(resultado (7.1))
-console.log(resultado (6.7))
-
-const status = nota >= 7 ? 'Aprovado' : 'Reprovado'
+// Operador ternario e composto por 3 parte
