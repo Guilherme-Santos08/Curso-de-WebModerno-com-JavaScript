3657d941f8235eaf2c2ceb70b201af4371981f66
Guilherme Augusto <68301964+Guilherme-Santos08@users.noreply.github.com>
2021-01-03 02:07:50 -0300

    doWhile

--- JavaScript Fundamentos/2-Controle/doWhile.js
+++ JavaScript Fundamentos/2-Controle/doWhile.js
@@ -1,13 +1,13 @@
-function getInteiroAleatorioEntre(min, max){
+function getInteiroAleatorioEntre(min, max) {
   const valor = Math.random() * (max - min) + min
   return Math.floor(valor)
 }
 
 let opcao = -1
 
-do{
+do {
   opcao = getInteiroAleatorioEntre(-1, 10)
-  console.log(`Opção escolhida foi ${opcao}.`)
+  console.log(`Opção escolhida foi ${opcao}`)
 } while (opcao != -1)
 
-console.log('Até a próxima!')
+console.log('Até a próxima')
