9b1addd745f50e6eadab0925ae38b99326c73eff
Guilherme Augusto <68301964+Guilherme-Santos08@users.noreply.github.com>
2021-01-03 00:37:02 -0300

    ifElse

--- JavaScript Fundamentos/2-Controle/3- ifElse.js
+++ JavaScript Fundamentos/2-Controle/3- ifElse.js
@@ -1,12 +1,11 @@
-const imprimirResultado = function (nota) {
-  if(nota >= 7) {
+const imprimirResultado = function(nota) {
+  if (nota >= 7) {
     console.log('Aprovado!')
-    //console.log(`Aprovado com  nota ${nota}`)
   } else {
-    console.log('Reprovado!')
+    console.log('Reprovado')
   }
 }
 
 imprimirResultado(10)
 imprimirResultado(4)
-imprimirResultado('Epa!') // cuidado
+imprimirResultado('epa!') // cuidado!!!, vai dar reprovado
