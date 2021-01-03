e08ca219468b8ca972fd7513b67face22ee8ecf8
Guilherme Augusto <68301964+Guilherme-Santos08@users.noreply.github.com>
2021-01-03 00:59:19 -0300

    isElseIf

--- JavaScript Fundamentos/2-Controle/isElself.js
+++ JavaScript Fundamentos/2-Controle/isElself.js
@@ -1,19 +1,24 @@
 Number.prototype.entre = function (inicio, fim) {
-  return this >= inicio && this <= fim
+    return this >= inicio && this <=fim
 }
 
 const imprimirResultado = function (nota) {
-  if (nota.entre(9, 10)) {
-      console.log('Quadro de Honra')
-  } else if (nota.entre(7, 8.99)) {
-      console.log('Aprovado')
-  } else if (nota.entre(4, 6.99)) {
-      console.log('Recuperação')
-  } else if (nota.entre(0, 3.99)) {
-      console.log('Reprovado')
-  } else {
-      console.log('Nota inválida')
-  }
+    if(nota.entre(9.10)) {
+        console.log('Quadro de Honra') // tomar cuidado que se não tiver o "else if" ele vai parar aqui e não vai mais imprimir
+    
+    }else if (nota.entre(7, 8.99)){
+        console.log('aprovado')
+    
+    }else if (nota.entre((4, 6.99))) {
+        console.log('Recuperação')
+    
+    }else if(nota.entre(0, 3.99)){
+        console.log('Reprovado')
+    
+    }else {
+        console.log('Nota inválida')
+    }
+    // console.log('Fim') // Sempre que ele sair de um bloco, irar aparecer "Fim"
 }
 
 imprimirResultado(10)
