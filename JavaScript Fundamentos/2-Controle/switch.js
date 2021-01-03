60e3a3fcd1070cac5ddeec2ddaa74cfcf4436019
Guilherme Augusto <68301964+Guilherme-Santos08@users.noreply.github.com>
2021-01-03 01:22:38 -0300

    switch

--- JavaScript Fundamentos/2-Controle/switch.js
+++ JavaScript Fundamentos/2-Controle/switch.js
@@ -1,9 +1,9 @@
 const imprimirResulatado = function (nota) {
-  switch (Math.floor(nota)) {
+  switch (Math.floor(nota)) { // floor aredonda pra baixo
     case 10:
     case 9:
       console.log('Quadro de Honra')
-      break
+      break // sempre que colocarmos case vamos precisar colocar o switch, se não colocarmos ele vai continuar lendo os outros case.
     case 8: case 7:
       console.log('Aprovado')
       break
