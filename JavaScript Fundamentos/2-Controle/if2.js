25df9f75f84947a1e095b97e460e381eedef8a95
Guilherme Augusto <68301964+Guilherme-Santos08@users.noreply.github.com>
2021-01-03 00:04:36 -0300

    if

--- JavaScript Fundamentos/2-Controle/if2.js
+++ JavaScript Fundamentos/2-Controle/if2.js
@@ -1,8 +1,18 @@
 function teste1(num) {
   if(num > 7)
   console.log(num)
+
   console.log('Final')
 }
 
 teste1(6)
 teste1(8)
+
+function teste2(num) {
+  if(num > 7); { // não use ponto e virgula em estrutras if ou de controles, por conta de ponto e virgula, eles esta lendo o 6 e 8
+    console.log(num)
+  }
+}
+
+teste2(6)
+teste2(8)
