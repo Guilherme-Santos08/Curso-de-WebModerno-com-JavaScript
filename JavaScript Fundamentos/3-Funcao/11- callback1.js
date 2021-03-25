3ceaa6ed2095f13c013e9bfa13cebb993e7da3bd
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-25 17:02:10 -0300

    callback1

---
+++ JavaScript Fundamentos/3-Funcao/11- callback1.js
@@ -0,0 +1,9 @@
+const fabricantes = ["Mercades", "Audi", "Bmw"]
+
+function imprimir(nome, indice) {
+  console.log(`${indice + 1}. ${nome}`)
+}
+
+fabricantes.forEach(imprimir)
+fabricantes.forEach(fabricante => 
+  console.log(fabricante))
