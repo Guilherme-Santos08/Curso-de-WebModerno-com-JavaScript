df4835ef26b7caa7d7685bff385118dbf75f40e1
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-04-05 16:01:37 -0300

    4- objetosConstantes

--- JavaScript Fundamentos/4-Objeto/4- objetosConstantes.js
+++ JavaScript Fundamentos/4-Objeto/4- objetosConstantes.js
@@ -1,10 +1,10 @@
 // pessoa -> 123 -> {...}
 const pessoa = { nome: 'joao'}
-pessoa.nome = 'Pedro'
+pessoa.nome = 'Pedro' // estou atribuindo para pessoa.nome, estou mudando o "nome" e não a "pessoa"
 console.log(pessoa)
 
 // pessoa -> 456 -> {...}
-// pessoa = {nome: 'Ana'}
+// pessoa = {nome: 'Ana'} Não posso atribuir novamente para constante pessoa
 
 Object.freeze(pessoa)
 
