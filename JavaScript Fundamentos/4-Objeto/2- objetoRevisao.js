3a9566cd5c2734418fc62d6c9f63a60b06735420
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-04-05 15:18:29 -0300

    2- objetoRevisao

--- JavaScript Fundamentos/4-Objeto/2- objetoRevisao.js
+++ JavaScript Fundamentos/4-Objeto/2- objetoRevisao.js
@@ -6,13 +6,13 @@ produto.preco = 220
 
 console.log(produto)
 delete produto.preco
-delete produto['marca do produto']
+delete produto ['marca do produto']
 console.log(produto)
 
 const carro = {
   modelo: 'A4',
   valor: 89000,
-  proprietraio: {
+  proprietario: {
     nome: 'Raul',
     idade: 56,
     endereco: {
@@ -20,25 +20,25 @@ const carro = {
       numero: 123
     }
   },
-   condutores: [{
-     nome: 'Junioe',
-     idade: 19
-   }, {
-     nome: 'Ana',
-     idade: 42
-   }],
-   cacularValorSeguro: function() {
-     //...
-   }
+  condutores: [{
+    nome: 'Junior',
+    idade: 19
+  }, {
+    nome: 'Ana',
+    idade: 42
+  }],
+  calcularValorSeguro: function() {
+    // ...
+  }
 }
 
-carro.proprietraio.endereco.numero = 1000
-carro['proprietraio']['endereco']['logradouro'] = 'Av Gigante'
+carro.proprietario.endereco.numero = 1000
+carro ['proprietario']['endereco']['logradouro'] = 'Av Gigante'
 console.log(carro)
 
 // delete carro.condutores
-delete carro.proprietraio.endereco
-delete carro.cacularValorSeguro
+delete carro.proprietario.endereco
+delete carro.calcularValorSeguro
 console.log(carro)
 console.log(carro.condutores)
 console.log(carro.condutores.length)
