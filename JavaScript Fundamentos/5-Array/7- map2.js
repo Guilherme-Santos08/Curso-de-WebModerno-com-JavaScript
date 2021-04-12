d4eb2231927ab38ef39abc931e53eb685dd17bdb
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-04-12 16:48:54 -0300

    map2

--- JavaScript Fundamentos/5-Array/7- map2.js
+++ JavaScript Fundamentos/5-Array/7- map2.js
@@ -1,4 +1,4 @@
-const carrinho = [
+ const carrinho = [
   '{ "nome": "Borracha", "preco": 3.45 }',
   '{ "nome": "Caderno", "preco": 13.90 }',
   '{ "nome": "Kit de Lapis", "preco": 41.22 }',
@@ -7,6 +7,7 @@ const carrinho = [
 
 // Retornar um array apenas com os preços 
 
-carrinho.forEach(function(preco, indice,) {
-  carrinho.forEach(preco => console.log(preco))
-})
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(resultado)
