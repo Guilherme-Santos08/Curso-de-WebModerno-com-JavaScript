751d45bf2a52c3b138a65b591a7fe33dabddcf0d
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2021-07-14 12:35:28 -0300

    webpack

---
+++ JavaScript Fundamentos/17- webpack/webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/principal.js",
+  output: {
+    filename: "principal.js",
+    path: __dirname + "/public",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "estilo.css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s?[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          // "style-loader", // Adicionar CSS a DOM injetando a tag <style>
+          "css-loader", // interpretar @import, url()...
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
